c7d216ec2adcfd7de82e906223472ca261dc09f7
camille griset <camilleflo@me.com>
2020-03-05 16:34:43 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+resources :doses, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :cocktails, only: [:create, :index, :new, :show]
+resources :cocktails, only: [:create, :index, :new, :show] do
+resources :doses, only: [:create, :new]
 
+end
 end
