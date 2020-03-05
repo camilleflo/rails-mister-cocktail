6aa9fd5f4d9b4766f36b59a50cb079619461afe7
camille griset <camilleflo@me.com>
2020-03-05 15:38:50 +0100

    ok

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+	belongs_to :cocktail
+	belongs_to :ingredient
+
+	validates :ingredient_id, presence: true, uniqueness: { scope: :cocktail_id}
+	validates :cocktail_id, presence: true
+	validates :description, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Light rum")
+Ingredient.create(name: "Champagne")
+Ingredient.create(name: "Coffee liqueur")
+Ingredient.create(name: "Creme de Cacao")
+Ingredient.create(name: "Pineapple juice")
+
